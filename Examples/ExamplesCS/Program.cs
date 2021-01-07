e6d404d18f4da983ff1e4322a3a44ab46e6a4d51
Jan Fridrichovsky <jan.fridrichovsky@thermofisher.com>
2021-01-07 20:26:58 +0100

    C Sharp examples

---
+++ Examples/ExamplesCS/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplesCS
+{
+	class A
+	{
+		int a;
+		public int b;
+	}
+
+	interface IA
+	{
+		int fcn();
+	}
+	struct SA
+	{
+		int fcn()
+		{
+			return 0;
+		}
+	}
+
+	abstract class AA
+	{
+		public abstract void fcn();
+		public void fcnI()
+		{
+			
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			A aa = new A();
+			aa.b = 0;
+			//IA ia = new IA();
+			//int x = ia.fcn();
+		}
+	}
+}
